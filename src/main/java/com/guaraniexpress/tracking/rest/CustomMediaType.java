35c771b6e451908d66115096cce2c590aa38f175
rafael.benegas@konecta.com.py <rafael.benegas@konecta.com.py>
2015-03-15 21:08:16 -0300

    Se agrega la clase CustomMediaType, para incluir el tipo application/json junto con el charset UTF8

---
+++ src/main/java/com/guaraniexpress/tracking/rest/CustomMediaType.java
@@ -0,0 +1,7 @@
+package com.guaraniexpress.tracking.rest;
+
+import javax.ws.rs.core.MediaType;
+
+public class CustomMediaType extends MediaType {
+	public static final String APPLICATION_JSON_UTF8 = "application/json;charset=UTF-8";
+}
