c5b45cf6982ae62d04af1beaffc24577d7909258
rafael.benegas@konecta.com.py <rafael.benegas@konecta.com.py>
2015-03-18 11:42:48 -0300

    Se crea el etity RolUsuario

---
+++ src/main/java/com/guaraniexpress/tracking/entities/application/RolUsuario.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.guaraniexpress.tracking.entities.application;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author raphapy
+ */
+@Entity
+@Table(name = "rol_usuario", catalog = "guaraniexpress", schema = "")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "RolUsuario.findAll", query = "SELECT r FROM RolUsuario r"),
+    @NamedQuery(name = "RolUsuario.findByIdRolUsuario", query = "SELECT r FROM RolUsuario r WHERE r.idRolUsuario = :idRolUsuario"),
+    @NamedQuery(name = "RolUsuario.findByRol", query = "SELECT r FROM RolUsuario r WHERE r.rol = :rol")})
+public class RolUsuario implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id_rol_usuario")
+    private Short idRolUsuario;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "rol")
+    private int rol;
+    @JoinColumn(name = "usuario", referencedColumnName = "id_usuario")
+    @ManyToOne(optional = false)
+    private Usuario usuario;
+
+    public RolUsuario() {
+    }
+
+    public RolUsuario(Short idRolUsuario) {
+        this.idRolUsuario = idRolUsuario;
+    }
+
+    public RolUsuario(Short idRolUsuario, int rol) {
+        this.idRolUsuario = idRolUsuario;
+        this.rol = rol;
+    }
+
+    public Short getIdRolUsuario() {
+        return idRolUsuario;
+    }
+
+    public void setIdRolUsuario(Short idRolUsuario) {
+        this.idRolUsuario = idRolUsuario;
+    }
+
+    public int getRol() {
+        return rol;
+    }
+
+    public void setRol(int rol) {
+        this.rol = rol;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idRolUsuario != null ? idRolUsuario.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof RolUsuario)) {
+            return false;
+        }
+        RolUsuario other = (RolUsuario) object;
+        if ((this.idRolUsuario == null && other.idRolUsuario != null) || (this.idRolUsuario != null && !this.idRolUsuario.equals(other.idRolUsuario))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.guaraniexpress.tracking.entities.application.RolUsuario[ idRolUsuario=" + idRolUsuario + " ]";
+    }
+    
+}
